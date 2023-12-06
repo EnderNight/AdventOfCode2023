7c36cd65a9d64f598a24ab86b6efb3a0a36032ef
EnderNight <ndxendernight@gmail.com>
2023-12-06 11:45:58 +0100

    Finished Day5

--- day_6/part1.py
+++ day_6/part1.py
@@ -1,1 +1,30 @@
 #!/usr/bin/env python3
+
+def get_num_wins(time: int, dist: int):
+    shortest = 0
+    longest = 0
+
+    tmp_time = 0
+    while shortest < dist and tmp_time < time - 1:
+        tmp_time += 1
+        speed = tmp_time
+        remaining = time - tmp_time
+        shortest = remaining * speed
+    shortest = tmp_time
+    longest = time - shortest
+
+    return longest - shortest + 1
+
+
+with open("puzzle_input", "r") as input:
+    input = input.read().strip().splitlines()
+
+    times = input[0].split(":")[1].split()
+    dists = input[1].split(":")[1].split()
+
+    res = 1
+
+    for i in range(len(times)):
+        res *= get_num_wins(int(times[i]), int(dists[i]))
+
+    print("Result:", res)
--- day_6/part2.py
+++ day_6/part2.py
@@ -1,1 +1,27 @@
 #!/usr/bin/env python3
+
+def get_num_wins(time: int, dist: int):
+    shortest = 0
+    longest = 0
+
+    tmp_time = 0
+    while shortest < dist and tmp_time < time - 1:
+        tmp_time += 1
+        speed = tmp_time
+        remaining = time - tmp_time
+        shortest = remaining * speed
+    shortest = tmp_time
+    longest = time - shortest
+
+    return longest - shortest + 1
+
+
+with open("puzzle_input", "r") as input:
+    input = input.read().strip().splitlines()
+
+    time = int(input[0].split(":")[1].replace(" ", ""))
+    dist = int(input[1].split(":")[1].replace(" ", ""))
+
+    res = get_num_wins(time, dist)
+
+    print("Result:", res)
